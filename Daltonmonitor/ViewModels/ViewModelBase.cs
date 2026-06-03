be3881576e39a84567f3fff9d38dc794a3181e03
Arhi Games <arvid.hilgers@gmx.de>
2026-06-03 19:16:24 +0200

    Initial commit

---
+++ Daltonmonitor/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Daltonmonitor.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ Daltonmonitor/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Daltonmonitor.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
