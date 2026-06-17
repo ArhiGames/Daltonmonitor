95864d5e27ffbe8689e81c3ef085c30af01a162f
gallfeder <finnick4gallfeder@gmail.com>
2026-06-17 16:34:58 +0200

    [DISPLAY] Made number of days to display a JS variable and changed layout if there is only one day displayed

--- Daltonmonitor/Assets/script.js
+++ Daltonmonitor/Assets/script.js
@@ -1,29 +1,34 @@
 
+const maxNumberOfDaysToDisplay = 2;
+
 window.addEventListener("load", () => {
-    checkVisibleDays()
+    checkVisibleDays();
     setInterval(checkVisibleDays, 1000 * 60 * 15);
 })
 
 function checkVisibleDays() {
-    console.log("Checking for past days...")
-    const currentDate = new Date()
-    const currentDateString = String(currentDate.getFullYear() + addLeadingZero(currentDate.getMonth() + 1) + addLeadingZero(currentDate.getDate()))
-    const currentDateNumber =  Number(currentDateString)
-    console.log(`Current date is: ${currentDateNumber}`)
+    console.log("Checking for past days...");
+    const currentDate = new Date();
+    const currentDateString = String(currentDate.getFullYear() + addLeadingZero(currentDate.getMonth() + 1) + addLeadingZero(currentDate.getDate()));
+    const currentDateNumber =  Number(currentDateString);
+    console.log(`Current date is: ${currentDateNumber}`);
     
-    let countVisibleDays = 0
+    let countVisibleDays = 0;
     document.querySelectorAll(".day").forEach(day => {
-        if (Number(day.dataset.date) < currentDateNumber || countVisibleDays >= 2) {
-            day.style.display = "none"
+        if (Number(day.dataset.date) < currentDateNumber || countVisibleDays >= maxNumberOfDaysToDisplay) {
+            day.style.display = "none";
         } else {
-            countVisibleDays++
+            countVisibleDays++;
         }
     })
+    if (countVisibleDays === 1) {
+        document.body.classList.add("single-day");
+    }
 }
 
 function addLeadingZero(number) {
     if (String(number).length === 2) {
-        return String(number)
+        return String(number);
     }
-    return "0" + number
+    return "0" + number;
 }
